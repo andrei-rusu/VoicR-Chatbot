541b381b73b015e452cdff429b7b9e7a71bc6cfe
Andrei Rusu <rusu.c.andrei@gmail.com>
2018-07-24 15:06:51 +0300

    Add TTS ability. Add current location service. Add cleanup resources. Fixed small bugs

--- app/src/main/java/com/example/uia93237/chatbot/MainActivity.java
+++ app/src/main/java/com/example/uia93237/chatbot/MainActivity.java
@@ -1,16 +1,22 @@
 package com.example.uia93237.chatbot;
 
+import android.Manifest;
 import android.content.Context;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.media.MediaPlayer;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
 import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -19,18 +25,27 @@ import android.view.animation.AnimationUtils;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
+import android.widget.Toast;
 
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Map;
 
 import ai.api.AIDataService;
 import ai.api.AIListener;
 import ai.api.AIServiceException;
 import ai.api.android.AIConfiguration;
 import ai.api.android.AIService;
+import ai.api.model.AIContext;
 import ai.api.model.AIError;
 import ai.api.model.AIRequest;
 import ai.api.model.AIResponse;
@@ -39,14 +54,25 @@ import ai.api.model.Result;
 
 public class MainActivity extends AppCompatActivity implements AIListener {
 
-    private final static String accessToken = "f71f8955cc7a4003995542bb46f4f9e4 ";
+    private final static String accessToken = "aa61d172c55d430c8c958e2312e1658c";
     private final static String botName = "Anastasia";
     private final static String userName = "User";
+    private final static int PERMISSION_CODE = 1;
 
     private EditText editText;
 
+    private TextToSpeech tts;
+
+    // Firebase DB
     private DatabaseReference ref;
+
+    // Firebase Recycler Adapter
+    private FirebaseRecyclerAdapter<Message, ChatViewHolder> adapter;
+
+    // Dialogflow service
     private AIService aiService;
+    private AIDataService aiDataService;
+
 
     // boolean used to switch between the 2 pictograms in the fab button
     boolean flagFab = true;
@@ -55,9 +81,12 @@ public class MainActivity extends AppCompatActivity implements AIListener {
     @Override
     protected void onCreate(Bundle savedInstanceState){
 
+        super.onCreate(savedInstanceState);
+
+        // Request Audio Permission
+        ActivityCompat.requestPermissions(this, new String[] { Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.RECORD_AUDIO }, PERMISSION_CODE);
 
         // Set up view
-        super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         RecyclerView recyclerView = findViewById(R.id.recyclerView);
@@ -68,6 +97,13 @@ public class MainActivity extends AppCompatActivity implements AIListener {
         final LinearLayoutManager linearLayoutManager=new LinearLayoutManager(this);
         linearLayoutManager.setStackFromEnd(true);
         recyclerView.setLayoutManager(linearLayoutManager);
+
+        // Set up TTS
+        tts = new TextToSpeech(getApplicationContext(), (status) -> {
+            if(status == TextToSpeech.SUCCESS) {
+                tts.setLanguage(Locale.UK);
+            }
+        });
 
 
         // Set up Firebase
@@ -82,13 +118,32 @@ public class MainActivity extends AppCompatActivity implements AIListener {
         aiService = AIService.getService(this, config);
         aiService.setListener(this);
 
-        final AIDataService aiDataService = new AIDataService(config);
+        aiDataService = new AIDataService(config);
 
+        // Inititialize request to DialogFlow
         final AIRequest aiRequest = new AIRequest();
-
+        // Set location related context
+        FusedLocationProviderClient mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+        try {
+            mFusedLocationClient.getLastLocation().addOnSuccessListener((location) -> {
+                if (location != null) {
+                    AIContext currentLoc = new AIContext("weather");
+                    Map<String, String> locMap = new HashMap<>();
+                    locMap.put("latitude", String.valueOf(location.getLatitude()));
+                    locMap.put("longitude", String.valueOf(location.getLongitude()));
+                    currentLoc.setParameters(locMap);
+                    aiRequest.setContexts(Collections.singletonList(currentLoc));
+                }
+            });
+        } catch(SecurityException e) {
+            Log.d("EXCEPTION: ", e.getMessage());
+        }
 
         // Set up send message button listener
         addBtn.setOnClickListener(view -> {
+
+            view.playSoundEffect(android.view.SoundEffectConstants.CLICK);
+
             String message = editText.getText().toString().trim();
 
             if (!message.equals("")) {
@@ -98,10 +153,10 @@ public class MainActivity extends AppCompatActivity implements AIListener {
 
                 aiRequest.setQuery(message);
 
-                new QueryTask(aiDataService, ref).execute(aiRequest);
+                new QueryTask().execute(aiRequest);
 
             }
-            else {
+            else { // the else block takes care of voice input
                 aiService.startListening();
             }
 
@@ -111,9 +166,7 @@ public class MainActivity extends AppCompatActivity implements AIListener {
 
         // Configure listener for edit text in order to change the pic
         editText.addTextChangedListener(new TextWatcher() {
-            @Override public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-
-            }
+            @Override public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
 
             @Override public void onTextChanged(CharSequence s, int start, int before, int count) {
                 ImageView fab_img = findViewById(R.id.fab_img);
@@ -129,9 +182,7 @@ public class MainActivity extends AppCompatActivity implements AIListener {
                 }
             }
 
-            @Override public void afterTextChanged(Editable s) {
-
-            }
+            @Override public void afterTextChanged(Editable s) {}
         });
 
 
@@ -145,7 +196,7 @@ public class MainActivity extends AppCompatActivity implements AIListener {
                         .setQuery(ref.child("chat"), Message.class)
                         .build();
 
-        FirebaseRecyclerAdapter<Message, ChatViewHolder> adapter = new FirebaseRecyclerAdapter<Message, ChatViewHolder>(options){
+        adapter = new FirebaseRecyclerAdapter<Message, ChatViewHolder>(options){
 
             @Override
             protected void onBindViewHolder(@NonNull ChatViewHolder holder, int position, @NonNull Message model) {
@@ -198,6 +249,22 @@ public class MainActivity extends AppCompatActivity implements AIListener {
     }
 
 
+    // handles the response coming back from the Google Dialogflow engine after being analysed
+    private void handleResponse(AIResponse response) {
+
+        Result result = response.getResult();
+        String reply = result.getFulfillment().getSpeech();
+
+        // set text bubble for bot
+        Message chatMessageBot = new Message(reply, botName);
+        ref.child("chat").push().setValue(chatMessageBot);
+
+        // TTS
+        tts.speak(reply, TextToSpeech.QUEUE_ADD, null, null);
+
+    }
+
+
     /*
     AIListener interface implementation
      */
@@ -205,17 +272,13 @@ public class MainActivity extends AppCompatActivity implements AIListener {
     @Override
     public void onResult(AIResponse response) {
 
-        Result result = response.getResult();
-
-        String message = result.getResolvedQuery();
+        // set the text bubble for the user query
+        String message = response.getResult().getResolvedQuery();
         Message chatMessageUser = new Message(message, userName);
         ref.child("chat").push().setValue(chatMessageUser);
 
-
-        String reply = result.getFulfillment().getSpeech();
-        Message chatMessageBot = new Message(reply, botName);
-        ref.child("chat").push().setValue(chatMessageBot);
-
+        // handle the response, including the creation of the text bubble for the bot
+        handleResponse(response);
     }
 
     @Override
@@ -249,10 +312,8 @@ public class MainActivity extends AppCompatActivity implements AIListener {
                                                final ImageView v, final Bitmap new_image) {
         final Animation anim_out = AnimationUtils.loadAnimation(c,
                 android.R.anim.fade_out);
-//        anim_out.setDuration(2000);
         final Animation anim_in = AnimationUtils.loadAnimation(c,
                 android.R.anim.fade_in);
-//        anim_in.setDuration(2000);
         anim_out.setAnimationListener(new Animation.AnimationListener() {
             @Override
             public void onAnimationStart(Animation animation) {
@@ -284,36 +345,81 @@ public class MainActivity extends AppCompatActivity implements AIListener {
         v.startAnimation(anim_out);
     }
 
-    // AsyncTask that will complete the query to the AIDataService, remembering the result in Firebase
 
-    private static class QueryTask extends AsyncTask<AIRequest,Void,AIResponse> {
+    // Code that executes after a permission is granted
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String permissions[], @NonNull int[] grantResults) {
 
-        private AIDataService dataService;
-        private DatabaseReference ref;
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
 
-        private QueryTask(AIDataService dataService, DatabaseReference ref) {
-            this.dataService = dataService;
-            this.ref = ref;
+        if (requestCode == PERMISSION_CODE) {
+
+            for (int i = 0; i < permissions.length; i++) {
+
+                String permission = permissions[i];
+                int grantResult = grantResults[i];
+
+                switch(permission) {
+                    case Manifest.permission.ACCESS_FINE_LOCATION:
+                        if (grantResults[i] == PackageManager.PERMISSION_GRANTED) {
+                            Toast.makeText(this,"GPS permission granted",Toast.LENGTH_LONG).show();
+                        }
+                        else {
+                            Toast.makeText(this,"GPS permission denied",Toast.LENGTH_LONG).show();
+                        }
+                        break;
+                    case Manifest.permission.RECORD_AUDIO:
+                        if (grantResult == PackageManager.PERMISSION_GRANTED) {
+                            Toast.makeText(this,"Audio recording permission granted",Toast.LENGTH_LONG).show();
+                        }
+                        else {
+                            Toast.makeText(this,"Audio recording permission denied",Toast.LENGTH_LONG).show();
+                        }
+                }
+            }
+
         }
+    }
+
+
+    // Cleanup Resources
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+
+        aiService.stopListening();
+        tts.stop();
+        adapter.stopListening();
+    }
+
+
+
+    /*
+     AsyncTask that will complete the query to the AIDataService, remembering the result in Firebase
+      */
+
+    private class QueryTask extends AsyncTask<AIRequest,Void,AIResponse> {
+
+        private QueryTask() {}
 
         @Override
         protected AIResponse doInBackground(AIRequest... aiRequests) {
 
             final AIRequest req = aiRequests[0];
             try {
-                return dataService.request(req);
-            } catch (AIServiceException e) {}
+                return aiDataService.request(req);
+            } catch (AIServiceException e) {
+                Log.d("EXCEPTION: ", e.getMessage());
+            }
             return null;
         }
         @Override
         protected void onPostExecute(AIResponse response) {
             if (response != null) {
 
-                Result result = response.getResult();
-                String reply = result.getFulfillment().getSpeech();
-                Message chatMessage = new Message(reply, botName);
-                ref.child("chat").push().setValue(chatMessage);
+                handleResponse(response);
             }
         }
     }
+
 }
